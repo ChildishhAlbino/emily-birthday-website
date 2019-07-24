b571ec7ca2227b5d0561f05521cc53db64695f3f
Connor <connorwilliams.home@gmail.com>
2019-07-25 03:09:47 +1000

    [Connor] Lowercase image import. Remove Scss file.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import KDAEmily from '../images/KDAEmily.png';
-import './index.scss';
+import KDAEmily from '../images/kdaemily.png';
 const IndexPage = () => (
 	<div>
 		<h1>HAPPY BIRTHDAY EMILY</h1>
-		<img className="image" src={KDAEmily} alt="KDAEmily" />
+		<img src={KDAEmily} alt="KDAEmily" />
 		<p>
 			Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident architecto minima debitis impedit tempora
 			minus totam facere illum, quis pariatur dolorem illo. Nostrum reiciendis impedit facilis possimus quas, iste
