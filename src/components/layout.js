f2189d499bd6ace066f4f82080abed96e04cfb5d
Connor <connorwilliams.home@gmail.com>
2019-07-26 22:35:37 +1000

    [Connor] Add timer.

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,12 +1,13 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 import './layout.scss';
 import PageTransition from './transition';
 import { useStaticQuery, graphql } from 'gatsby';
 import Arrows from './arrows';
+import moment from 'moment';
+import Timer from '../components/timer';
 
 const Context = React.createContext();
-
+const countdownDate = moment('2019-07-27');
 class ContextProvider extends React.Component {
 	state = {
 		animation: null,
@@ -20,51 +21,55 @@ class ContextProvider extends React.Component {
 }
 
 const Layout = ({ children, location }) => {
-	const pages = [];
-	const data = useStaticQuery(graphql`
-		query MyQuery {
-			allSitePage {
-				edges {
-					node {
-						path
+	if (moment().isSameOrBefore(countdownDate)) {
+		return (
+			<div>
+				<Timer countdownDate={countdownDate} />
+			</div>
+		);
+	} else {
+		const pages = [];
+		const data = useStaticQuery(graphql`
+			query MyQuery {
+				allSitePage {
+					edges {
+						node {
+							path
+						}
 					}
 				}
 			}
+		`);
+		data.allSitePage.edges.forEach((element) => {
+			const path = element.node.path;
+			if (!path.includes('404')) {
+				console.log(path);
+				pages.push(path);
+			}
+		});
+		const pageIndex = pages.indexOf(location.pathname);
+		let nextPageIndex = pageIndex + 1;
+		let previousPageIndex = pageIndex - 1;
+		if (pageIndex === 0) {
+			previousPageIndex = pages.length - 1;
 		}
-	`);
-	data.allSitePage.edges.forEach((element) => {
-		const path = element.node.path;
-		if (!path.includes('404')) {
-			console.log(path);
-			pages.push(path);
+		if (pageIndex === pages.length - 1) {
+			nextPageIndex = 0;
 		}
-	});
-	const pageIndex = pages.indexOf(location.pathname);
-	let nextPageIndex = pageIndex + 1;
-	let previousPageIndex = pageIndex - 1;
-	if (pageIndex === 0) {
-		previousPageIndex = pages.length - 1;
+		if (!location.state) {
+			location.state = {};
+		}
+		return (
+			<ContextProvider>
+				<div className="container">
+					<PageTransition location={location}>
+						<div className="content">{children}</div>
+					</PageTransition>
+					<Arrows className="overlay" next={pages[nextPageIndex]} previous={pages[previousPageIndex]} />
+				</div>
+			</ContextProvider>
+		);
 	}
-	if (pageIndex === pages.length - 1) {
-		nextPageIndex = 0;
-	}
-	if (!location.state) {
-		location.state = {};
-	}
-	return (
-		<ContextProvider>
-			<div className="container">
-				<PageTransition location={location}>
-					<div className="content">{children}</div>
-				</PageTransition>
-				<Arrows className="overlay" next={pages[nextPageIndex]} previous={pages[previousPageIndex]} />
-			</div>
-		</ContextProvider>
-	);
-};
-
-Layout.propTypes = {
-	children: PropTypes.node.isRequired
 };
 
 export default Layout;
---
+++ src/components/timer.js
@@ -0,0 +1,57 @@
+import React from 'react';
+import moment from 'moment';
+import './timer.scss';
+
+class CountdownTimer extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			countdownDate: props.countdownDate,
+			diff: this.calcDiff(moment(), props.countdownDate)
+		};
+	}
+
+	componentDidMount() {
+		setInterval(this.tick.bind(this), 1000);
+	}
+
+	calcDiff(now, countdownDate) {
+		const rawDiff = countdownDate.diff(now, 'seconds');
+		const diff = { days: 0, hours: 0, minutes: 0, seconds: 0 };
+		diff.days = Math.floor(rawDiff / (60 * 60 * 24));
+		diff.hours = Math.floor((rawDiff % (60 * 60 * 24)) / (60 * 60));
+		diff.minutes = Math.floor((rawDiff % (60 * 60)) / 60);
+		diff.seconds = Math.floor(rawDiff % 60);
+		return diff;
+	}
+
+	tick() {
+		const diff = this.calcDiff(moment(), this.state.countdownDate);
+		this.setState({ diff: diff });
+	}
+
+	suffix(value, singular) {
+		if (value === 1) {
+			return `${value} ${singular}`;
+		} else {
+			return `${value} ${singular + 's'}`;
+		}
+	}
+
+	render() {
+		const days = this.suffix(this.state.diff.days, 'day');
+		const hours = this.suffix(this.state.diff.hours, 'hour');
+		const minutes = this.suffix(this.state.diff.minutes, 'minute');
+		const seconds = this.suffix(this.state.diff.seconds, 'second');
+		return (
+			<div className="timer">
+				<h1>{days},</h1>
+				<h1>{hours},</h1>
+				<h1>{minutes},</h1>
+				<h1>{seconds}.</h1>
+			</div>
+		);
+	}
+}
+
+export default CountdownTimer;
