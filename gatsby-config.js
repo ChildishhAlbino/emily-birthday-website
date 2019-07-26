c00792bd8d71586b953cd7cdae2c4cb43280ea01
Connor <connorwilliams.home@gmail.com>
2019-07-27 01:13:01 +1000

    [Connor] Update Gatsby Config.

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,9 +2,9 @@ const path = require(`path`);
 
 module.exports = {
 	siteMetadata: {
-		title: `Gatsby Default Starter`,
-		description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-		author: `@gatsbyjs`
+		title: `Happy Birthday Emily!!!`,
+		description: `A Birthday Site for Emily!!`,
+		author: `@ChildishhAlbino`
 	},
 	plugins: [
 		{
@@ -33,17 +33,6 @@ module.exports = {
 		`gatsby-transformer-remark`,
 		`gatsby-transformer-sharp`,
 		`gatsby-plugin-sharp`,
-		{
-			resolve: `gatsby-plugin-manifest`,
-			options: {
-				name: `gatsby-starter-default`,
-				short_name: `starter`,
-				start_url: `/`,
-				background_color: `#663399`,
-				theme_color: `#663399`,
-				display: `minimal-ui`
-			}
-		},
 		{
 			resolve: `gatsby-plugin-layout`,
 			options: {
