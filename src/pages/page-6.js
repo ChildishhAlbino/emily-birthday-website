1029959d78a13ed1a6990a7e3d94d7c4afa03d95
Connor <connorwilliams.home@gmail.com>
2019-07-26 19:48:42 +1000

    [Connor] Add pages-4 through 9

---
+++ src/pages/page-6.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const SixthPage = () => (
+	<div className="main">
+		<h1>THIS IS THE SIXTH PAGE</h1>
+	</div>
+);
+
+export default SixthPage;
---
+++ src/pages/page-9.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const NinthPage = () => (
+	<div className="main">
+		<h1>THIS IS THE NINTH PAGE</h1>
+	</div>
+);
+
+export default NinthPage;
