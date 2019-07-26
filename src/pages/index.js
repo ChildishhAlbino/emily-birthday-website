a3b661a534d459d463df64d2b5cf36c5be409cc9
Connor <connorwilliams.home@gmail.com>
2019-07-26 19:48:30 +1000

    [Connor] Remove image from page one.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,7 @@
 import React from 'react';
-import KDAEmily from '../images/kda-emily.png';
 const IndexPage = () => (
 	<div>
-		<h1>HAPPY BIRTHDAY EMILY</h1>
-		<img src={KDAEmily} alt="KDAEmily" />
-		<p>
-			Lorem ipsum dolor sit amet consectetur adipisicing elit. Provident architecto minima debitis impedit tempora
-			minus totam facere illum, quis pariatur dolorem illo. Nostrum reiciendis impedit facilis possimus quas, iste
-			aliquid!
-		</p>
+		<h1>THIS IS THE FIRST PAGE</h1>
 	</div>
 );
 
