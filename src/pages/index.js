af1f61baf493ff24374bb9bae06180961a6c7705
Connor <connorwilliams.home@gmail.com>
2019-07-28 00:29:24 +1000

    [Connor] Finish index page.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,28 +1,28 @@
 import React from 'react';
-import { useStaticQuery, graphql } from 'gatsby';
-import Markdown from '../markdown/test.mdx';
+import birthdayImage from '../images/birthday-page-gif.gif';
 const IndexPage = () => {
-	const data = useStaticQuery(graphql`
-		query PageOneMarkdownQuery {
-			image: file(relativePath: { eq: "kda-emily.png" }) {
-				childImageSharp {
-					fluid(maxWidth: 2000) {
-						...GatsbyImageSharpFluid
-					}
-				}
-			}
-			post: file(relativePath: { regex: "/test.mdx/" }) {
-				childMdx {
-					rawBody
-					body
-				}
-			}
-		}
-	`);
-
 	return (
 		<div>
-			<Markdown />
+			<h1>
+				<i>Happy 20th Birthday Emily!!!!</i>
+			</h1>
+			<p>
+				It's not every day one of your all-time best friends turns 20! You've been an amazing friend and someone
+				I've come to truly respect.
+			</p>
+			<p>
+				Our friendship has lasted a long time; through good and bad, and I'm so glad I've been allowed to see
+				you grow as a person for many years, and I hope to continue to do so for so many more to come.
+			</p>
+			<p>
+				A little birdie told me that you're a big fan of Eevee and personality traits so I thought I'd combine
+				the two!
+			</p>
+			<p>
+				So click around enjoy! Right is forwards, left is backwards. (There might be a special surprise for you
+				on the last slide)
+			</p>
+			<img className="image" src={birthdayImage} alt="Birthday Pikachu and Togepi." />
 		</div>
 	);
 };
