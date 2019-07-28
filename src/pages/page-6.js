39805bd9d443865ec42201be27dd2292b880371f
Connor <connorwilliams.home@gmail.com>
2019-07-29 01:36:13 +1000

    [Connor] Post page 6 draft

--- src/pages/page-6.js
+++ src/pages/page-6.js
@@ -18,6 +18,21 @@ const SixthPage = () => {
 		<div className="page">
 			<h1>UMBREON</h1>
 			<Image className="gatsbyImage" alt={'Eevee'} fluid={data.image.childImageSharp.fluid} />
+			<div>
+				<div>
+					<h3>Dark Type Pokemon | Related Star Signs: Aquarius, Gemini</h3>
+				</div>
+				<p>
+					Ambition and Ambivertedness are found under the sign of the dark type eveelution - Umbreon. Umbreon
+					isn't nefarious by nature, they just know what they want. They aren't particularly social, but they
+					aren't antisocial either. Umbreon stans are exactly what the situation calls of them.
+				</p>
+				<p>
+					Umbreon stans should be careful of tunnel-visoning too heavily on success during Generation
+					Eighgtrograde as it might lead to problematic relationships in the future. Flareon stans are the
+					perfect companion throughout this hard time.
+				</p>
+			</div>
 		</div>
 	);
 };
