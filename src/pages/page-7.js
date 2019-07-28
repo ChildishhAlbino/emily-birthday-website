e9c65b41099fffff1b757ff80bb79de84252c00d
Connor <connorwilliams.home@gmail.com>
2019-07-29 01:59:00 +1000

    [Connor] Post page seven

--- src/pages/page-7.js
+++ src/pages/page-7.js
@@ -17,6 +17,23 @@ const SeventhPage = () => {
 		<div className="page">
 			<h1>LEAFEON</h1>
 			<Image className="gatsbyImage" alt={'Eevee'} fluid={data.image.childImageSharp.fluid} />
+			<div>
+				<div>
+					<h3>Grass Type Pokemon | Related Star Signs: Scorpio, Pisces</h3>
+				</div>
+				<p>
+					Leafeon is the sign of affection. Humans who align with Leafeon's qualities are likely to be the
+					most naturally empathetic and emotionally expressive. Leafeon stans are of the grass element and
+					therefore, they have a special connection with nature. They are lively by nature and enjoy music,
+					but are not by any means the most desire-driven of all the signs. Leafeon stans are very often
+					content with happiness as they define it.
+				</p>
+				<p>
+					Leafeon stans should find a rock during Generation Eightrograde. Affection can be forgotten when
+					times get tough and those that do not are worth keeping. Leafeon stans work best when surrounded by
+					Espeon and Jolteon stans.
+				</p>
+			</div>
 		</div>
 	);
 };
