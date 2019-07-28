a916aa2228ab15c789f6219ba342873e46f18ca7
Connor <connorwilliams.home@gmail.com>
2019-07-29 00:59:51 +1000

    [Connor] Post page 2 final draft.

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -18,6 +18,22 @@ const SecondPage = () => {
 		<div className="page">
 			<h1>VAPOREON</h1>
 			<Image className="gatsbyImage" alt={'Eevee'} fluid={data.image.childImageSharp.fluid} />
+			<div>
+				<div>
+					<h3>Water Type Pokemon | Related Star Signs: Libra, Capricorn</h3>
+				</div>
+				<p>
+					The Vaporeon sign focuses on a calm demeanour, or level-headedness. You can always rely on someone
+					who resembles that of Vaporeon to be self-assured and carefully judge any situation that comes their
+					way. Vaporeon stans are usually of an older mind than those that do not share such prominence with
+					the water-type Eeveelution.
+				</p>
+				<p>
+					Vaporeon stans are the safest of all the other signs during the time of Generation Eightrograde as
+					they will always keep their head above water. You may experience some rough tides in the coming
+					months, but be sure of yourself and you will pull through.
+				</p>
+			</div>
 		</div>
 	);
 };
