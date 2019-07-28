25d705a36c129747586b12a764aad946cfbb6155
Connor <connorwilliams.home@gmail.com>
2019-07-29 01:07:52 +1000

    [Connor] Post page three draft

--- src/pages/page-3.js
+++ src/pages/page-3.js
@@ -18,6 +18,21 @@ const ThirdPage = () => {
 		<div className="page">
 			<h1>JOLTEON</h1>
 			<Image className="gatsbyImage" alt={'Eevee'} fluid={data.image.childImageSharp.fluid} />
+			<div>
+				<div>
+					<h3>Electric Type Pokemon | Related Star Signs: Sagittarius, Aries</h3>
+				</div>
+				<p>
+					Jolteon comes from electricity - a spark that drives people to do more than just live a normal life.
+					This Eeviac sign embodies the human quality of creativity and spontaneity. Jolteon stans are often
+					found sporadically making major life decisions and rolling with it.
+				</p>
+				<p>
+					Jolteon stans might suffer from a lack-of-forethought during Generation Eightrograde. Although they
+					are used to getting in over their heads, the consequences of such a careless decision could be
+					significant.
+				</p>
+			</div>
 		</div>
 	);
 };
