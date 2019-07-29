12211cc91a0da1c246d04200678374449c46bdb5
Connor <connorwilliams.home@gmail.com>
2019-07-29 23:04:36 +1000

    [Connor] Add Page 11 (KDA Emily Page)

---
+++ src/pages/page-11.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import Image from 'gatsby-image';
+import { useStaticQuery, graphql } from 'gatsby';
+const EleventhPage = () => {
+	const data = useStaticQuery(graphql`
+		query PageElevenMarkdownQuery {
+			image: file(relativePath: { eq: "kda-emily.png" }) {
+				childImageSharp {
+					fluid(maxWidth: 3940, quality: 100) {
+						...GatsbyImageSharpFluid
+					}
+				}
+			}
+		}
+	`);
+	return (
+		<div className="page">
+			<h1>INTRODUCING... K/DA EMILY</h1>
+			<Image className="gatsbyImage" alt={'Eevee'} fluid={data.image.childImageSharp.fluid} />
+			<p>Artwork by ShyGirl @ Sketchmob</p>
+			<a href="https://imgur.com/m83JUgg">Click here to open in a new tab to download.</a>
+		</div>
+	);
+};
+
+export default EleventhPage;
